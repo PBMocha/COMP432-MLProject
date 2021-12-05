eb3acc91e95799b6ea712319e852a60381e45d7a
David Rady <david.rady99@gmail.com>
2021-12-04 21:32:30 -0500

    interpretability

---
+++ interpretability/CifarClassifiers.py
@@ -0,0 +1,110 @@
+import torch
+from torch.nn.modules import activation
+import torch.optim
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+
+def train_cnn(trn_sets, model, epochs = 50, batch_size=1000, lr=0.05, momentum=0.9, weight_decay=0.001):
+    #model.train()
+    X_trn = trn_sets[0]
+    y_trn = trn_sets[1]
+
+    loss = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
+
+    for epoch in range(epochs):
+        for i in range(0, len(X_trn), batch_size):
+            X_b = X_trn[i:i+batch_size]
+            y_b = y_trn[i:i+batch_size]
+            y_pred = model(X_b)
+            l = loss(y_pred, y_b)
+            model.zero_grad()
+            l.backward()
+            optimizer.step()
+        
+        if epoch % 5 == 0:
+            print("Epoch %3d: training loss = %.4f" % (epoch, l.item()))
+        
+
+    return model
+
+def test_cnn(test_sets, model):
+    #model.eval()
+
+    X_tst = test_sets[0]
+    y_test = test_sets[1]
+
+    N = list(y_test.size())[0]
+
+    with torch.no_grad():
+
+        output = model(X_tst)
+
+        # Get higheest prob class
+        _, preds = torch.max(output, 1)
+        
+        avg_acc = (preds == y_test).float().mean().item()
+
+
+    
+    return preds, avg_acc
+
+def evaluate_decision_tree(trn_sets, tst_sets, param_grid, **kwargs):
+
+    X_trn, y_trn = trn_sets
+    X_tst, y_tst = tst_sets
+
+    tree_clf = DecisionTreeClassifier(**kwargs)
+    gs = GridSearchCV(tree_clf, param_grid=param_grid, verbose=1,cv=3, return_train_score=True).fit(X_trn, y_trn)
+
+    trn_acc = gs.best_estimator_.score(X_trn, y_trn)
+    tst_acc = gs.best_estimator_.score(X_tst, y_tst)
+
+    print(f" Best estimator{gs.best_estimator_}")
+    print(f"Train acc: {trn_acc}")
+    print(f"Test acc: {tst_acc}")
+
+    tree.plot_tree(gs.best_estimator_)
+    
+
+    return gs
+
+def activation_maximization(act_label, model, labels=[], step_slope=0.8, steps=10):
+
+    # data = np.zeros(shape=(1, 3, 32, 32))
+    loss = torch.nn.CrossEntropyLoss()
+    x = torch.ones((1, 3, 32, 32), requires_grad=True).float()
+    #x = torch.FloatTensor(data.astype('float32'), requires_grad=True)
+    act = torch.zeros(size=(1, 10))
+    act[0, labels.index(act_label)] = 1
+    #torch.reshape(act, ())
+    fig = plt.figure(figsize=(16, 16))
+    fig.suptitle(f"Activation of {act_label}")
+    for i in range(steps):
+
+        y = model(x)
+        
+        # print(act.shape)
+        #l = loss(act, y_act)
+        y.backward(act)
+        #l.backward()
+
+        with torch.no_grad():
+
+            x += step_slope*x.grad
+
+            x.grad.zero_()
+
+        fig.add_subplot(1, steps, i+1)
+        plt.imshow(x.detach().numpy().reshape(3, 32, 32).transpose(1, 2, 0))
+    plt.show()
+    
+
+
+    
+
+
+
---
+++ interpretability/CifarDataProcess.py
@@ -0,0 +1,122 @@
+import numpy as np
+import pickle as pck
+import os
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+import torch
+import matplotlib.pyplot as plt
+
+def load_file(file):
+    """
+    file = data file to load from
+    """
+
+    with open(file, 'rb') as f:
+        data = pck.load(f, encoding='bytes')
+
+    return data
+
+def load_training(folder):
+    """
+
+    loads all batch image training data into numpy array
+
+    folder = directory containing all the batch files
+    """
+
+    batches = [ f"data_batch_{i}" for i in range(1, 6)]
+
+    path = os.path.dirname(__file__)
+
+    folder = os.path.join(path, folder)
+
+    X = np.empty(shape=(10000, 3072))
+    y = np.empty(shape=10000)
+
+    for i, filename in enumerate(batches):
+        
+        data = load_file(folder + filename)
+        print(f"LOADED {data[b'batch_label']}")
+
+        if i == 0:
+            X = data[b'data']
+            y = data[b'labels']
+            continue
+        X = np.concatenate((X, data[b'data']), axis=0)
+        y = np.concatenate((y, data[b'labels']), axis=0)
+
+    return X, y
+
+def load_test(file):
+
+    path = os.path.dirname(__file__)
+
+    file = os.path.join(path, file)
+
+    X_tst = np.empty(shape=(10000, 3072))
+    y_tst = np.empty(shape=10000)
+
+    data = load_file(file)
+    X_tst = data[b'data']
+    y_tst = np.array(data[b'labels'])
+
+    return X_tst, y_tst
+
+def load_cifar10():
+    """
+    Returns: Tuple
+        X_train,  X_test = training and test image data
+        y_train, y_test = training and test labels
+        classes = classifcation names
+    """
+
+    X_trn, y_trn = load_training('./data/cifar-10-batches-py/')
+    X_tst, y_tst = load_test('./data/cifar-10-batches-py/test_batch')
+
+    dir = os.path.dirname(__file__)
+
+    path = os.path.join(dir, "./data/cifar-10-batches-py/batches.meta")
+
+    metadata = load_file(path)
+
+    label_names = np.ravel(metadata[b'label_names'])
+
+    return X_trn, X_tst, y_trn, y_tst, label_names
+
+def scale_data(X):
+    
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    scaler.fit(X)
+    return scaler.transform(X)
+
+def save_model(model):
+    filename = './out/cifar10.pth' 
+    torch.save(model.state_dict(), filename)
+    
+def load_model(model, file):
+    load_dict = torch.load(file)
+
+    model.load_state_dict(load_dict)
+
+    return model
+
+def plot_confusion_matrix(y_pred, y_true, classes = [], display_accs=True):
+
+    y_pred_names = [classes[i] for i in y_pred]
+    y_true_names = [classes[i] for i in y_true]
+
+    cm = confusion_matrix(y_true_names, y_pred_names, labels=classes)
+
+    if display_accs:
+        accuracies = cm.diagonal() / cm.sum(axis=1)
+        
+        for i, label in enumerate(classes):
+            print("accuracy of {}: {:.2%}".format(label.decode('ascii'), accuracies[i]))
+
+    d = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=classes)
+    d.plot(xticks_rotation='vertical')
+    plt.show()
+
+    # ConfusionMatrixDisplay.from_predictions(y_true, y_pred, labels=classes, xticks_rotation='vertical')
+
